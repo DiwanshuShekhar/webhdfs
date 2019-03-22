c596bf37e69b96123b2b42f5a33a25b26de1bbfd
Diwanshu Shekhar <diwanshu@gmail.com>
2019-03-22 14:36:41 -0400

    changed to setup tools for installation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 setup(name='webhdfs',
       version='0.0.1',
@@ -8,5 +9,14 @@ setup(name='webhdfs',
       description ='Python package to perform file operations on Kerberized HDFS',
       long_description='NA',
       requires=['webhdfs', 'webhdfs.requests_kerberos'],
-      packages=['webhdfs', 'webhdfs.requests_kerberos']
-      )
+      packages=['webhdfs', 'webhdfs.requests_kerberos'],
+      install_requires=[
+        'requests>=1.1.0',
+        'cryptography>=1.3;python_version!="3.3"',
+        'cryptography>=1.3,<2;python_version=="3.3"'
+      ],
+      extras_require={
+        ':sys_platform=="win32"': ['winkerberos>=0.5.0'],
+        ':sys_platform!="win32"': ['pykerberos>=1.1.8,<2.0.0'],
+      },
+)
